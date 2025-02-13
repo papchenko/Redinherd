8ae7b9d7d5df816e547541e999ad1a07fc764654
papchenko <nikopapchenko@gmail.com>
2025-02-13 23:45:00 +0200

    v 1.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from 'astro/config';
-
+import browserSync from 'vite-plugin-browser-sync';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://redinherd.vercel.app',
   output: 'server',
   adapter: vercel(),
+  vite: {
+    plugins: [
+      browserSync({
+        open: false,
+        host: '0.0.0.0',
+        port: 3000,
+        proxy: 'http://localhost:3000',
+      }),
+    ],
+  },
 });
