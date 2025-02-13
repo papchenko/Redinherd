353a4ca951ad02e6fd528a37aac71953f2b02e0c
papchenko <nikopapchenko@gmail.com>
2025-02-13 22:19:50 +0200

    v 1.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
-    site: 'https://papchenko.github.io',
-    base: '/Redinherd/',
+  site: 'https://redinherd.vercel.app',
+  output: 'server',
+  adapter: vercel(),
 });
