8580bb0fd26f4674fa7ca7e6de9ab18ae01482e7
papchenko <nikopapchenko@gmail.com>
2025-02-13 11:27:45 +0200

    v 1.0.0

--- src/js/main.js
+++ src/js/main.js
@@ -37,14 +37,34 @@ function scrollUp() {
 window.addEventListener('scroll', scrollUp);
 
 // gsap
-// gsap.from('.logo-intro-animation', {duration: 4.5, opacity: 0, ease: 'power2.in'});
-// gsap.from('.text-intro-animation', {duration: 2.5, opacity: 0, ease: 'power2.in'});
-// gsap.from('.social-intro-animation', {duration: 3.5, opacity: 0, ease: 'power2.in'});
-// gsap.from('.scroll-intro-animation', {duration: 5.5, opacity: 0, ease: 'power2.in'});
-// gsap.to(".deco-1-animation", {rotation: 360, y: 100, duration: 1});
-// gsap.to(".deco-4-animation", {rotation: 360, y: 100, duration: 1});
-// gsap.from('.deco-1-animation', {x: -100, duration: 1, opacity: 0, ease: 'power1.in'});
-// gsap.from('.deco-2-animation', {y: -100, duration: 1, opacity: 0, ease: 'power1.in'});
-// gsap.from('.deco-3-animation', {y: -50, duration: 1, opacity: 0, ease: 'power1.in'});
-// gsap.from('.deco-5-animation', {x: -150, duration: 1, opacity: 0, ease: 'power1.in'});
-// gsap.from('.deco-6-animation', {y: -150, duration: 1, opacity: 0, ease: 'power1.in'});
+import { gsap } from "gsap";
+gsap.from('.logo-intro-animation', {duration: 4.5, opacity: 0, ease: 'power2.in'});
+gsap.from('.text-intro-animation', {duration: 2.5, opacity: 0, ease: 'power2.in'});
+gsap.from('.social-intro-animation', {duration: 3.5, opacity: 0, ease: 'power2.in'});
+gsap.from('.scroll-intro-animation', {duration: 5.5, opacity: 0, ease: 'power2.in'});
+gsap.to(".deco-1-animation", {rotation: 360, y: 100, duration: 1});
+gsap.to(".deco-4-animation", {rotation: 360, y: 100, duration: 1});
+gsap.from('.deco-1-animation', {x: -100, duration: 1, opacity: 0, ease: 'power1.in'});
+gsap.from('.deco-2-animation', {y: -100, duration: 1, opacity: 0, ease: 'power1.in'});
+gsap.from('.deco-3-animation', {y: -50, duration: 1, opacity: 0, ease: 'power1.in'});
+gsap.from('.deco-5-animation', {x: -150, duration: 1, opacity: 0, ease: 'power1.in'});
+gsap.from('.deco-6-animation', {y: -150, duration: 1, opacity: 0, ease: 'power1.in'});
+
+// swiper
+import Swiper from "swiper";
+import "swiper/css";
+
+new Swiper('.new-swiper', {
+spaceBetween: 16,
+slidesPerView: "auto",
+loop: true,
+breakpoints: {
+    320: {
+    slidesPerView: 1.2,
+    centeredSlides: true,
+    },
+    768: {
+    slidesPerView: 3,
+    },
+},
+});
